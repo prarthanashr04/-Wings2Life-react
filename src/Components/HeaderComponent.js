e9d47ec740dc24187a63e4dba355ff1a9e5ede87
Prarthana <prarthanashr04@gmail.com>
2020-08-02 16:23:00 +0530

    .

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import $ from 'jquery';
 
 function Header(){
+  var icon="./skillpill.png";
     
     return (
-        <div className="Header">
+        <div className="Header" >
             <Navbar fixed="top" dark color ="dark" >
               <div className="container">
-                <NavbarBrand >SkillPill</NavbarBrand>
+                <NavbarBrand ><img  id="logo" height="70" width="70" />SkillPill</NavbarBrand>
+                
               </div>
+              
             </Navbar>
         </div>
+        
       );
-    
+    /*$("#logo").attr('src',icon);*/
 }
 
 export default Header;
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -2,7 +2,29 @@ import React ,{Component}from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Body from './BodyComponent';
+import $ from 'jquery';
+
 function Main(){
+    var configuration;
+    
+    configuration = {
+		branding: [{"name" : "SkillPill" }],
+		title: "Qualities",
+		category: "Baselines",
+		icon: "./skillpill.png",
+		url: "https://ix61k6qun9.execute-api.ap-southeast-1.amazonaws.com/prod/lifetoolsdataset",
+		levelDetails: [{count:12 ,rule: "minimum" }, 
+					   /*{count:8, rule: "exact"},
+					   {count:6, rule: "exact"},*/
+					   {count:4, rule: "exact"}]
+    }
+    
+    $("#favlogo").attr('href',configuration.icon);
+   
+    function icon(){
+        $("#logo").attr('src',configuration.icon);
+    }
+    
     return(
         <div className="container">
             <Header/>
@@ -10,6 +32,7 @@ function Main(){
             <Body />
         </div>
     );
+    
 }
 
 export default Main;
